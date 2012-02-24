ce4610dcdd00b90af33d9cedeae3fab3cf999cf7
Bertrand Paquet <bpaquet@octo.com>
2012-02-24 16:17:27 +0100

    Allow ssh root login

--- cookbooks/base/recipes/sshd.rb
+++ cookbooks/base/recipes/sshd.rb
@@ -1,13 +1,28 @@
 
 unless node[:no_sshd_config]
-  
-  bash "Configure sshd - disallow root login" do
-    user "root"
-    code <<-EOF
-    sed -i 's/^PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
-    /etc/init.d/ssh restart
-    EOF
-    only_if "egrep '^PermitRootLogin yes' /etc/ssh/sshd_config"
+
+  if node[:allow_ssh_root_login]
+
+     bash "Configure sshd - allow root login" do
+      user "root"
+      code <<-EOF
+      sed -i 's/^PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
+      /etc/init.d/ssh restart
+      EOF
+      only_if "egrep '^PermitRootLogin no' /etc/ssh/sshd_config"
+    end
+
+  else
+
+    bash "Configure sshd - disallow root login" do
+      user "root"
+      code <<-EOF
+      sed -i 's/^PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+      /etc/init.d/ssh restart
+      EOF
+      only_if "egrep '^PermitRootLogin yes' /etc/ssh/sshd_config"
+    end
+
   end
 
   bash "Configure sshd - disallow password" do
