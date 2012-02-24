67a8e1a144dfc077e733cf4465bb867b4df22419
Bertrand Paquet <bpaquet@octo.com>
2012-02-24 15:42:15 +0100

    Implement git repos management

--- runtime/solo.rb
+++ runtime/solo.rb
@@ -14,12 +14,43 @@ ENV['MASTER_CHEF_CONFIG'] = File.expand_path(config_file)
 
 puts "************************* Master chef solo SOLO *************************"
 puts "Hostname : #{`hostname`}"
-puts "Repos : #{config["repos"]}"
+puts "Repos : #{config["repos"].inspect}"
 puts "Run list : #{run_list.inspect}"
 puts "******************************************************************************"
+
+def exec_local cmd
+  begin
+    abort "#{cmd} failed. Aborting..." unless system cmd
+  rescue
+    abort "#{cmd} failed. Aborting..."
+  end
+end
+
+git_cache_directory = ENV["GIT_CACHE_DIRECTORY"] || "/var/chef/cache/git_repos"
+exec_local "mkdir -p #{git_cache_directory}"
+
+cookbooks = []
+roles = []
+
+config["repos"]["git"].each do |url|
+  name = File.basename(url)
+  target = File.join(git_cache_directory, name)
+  unless File.exists? target
+    puts "Cloning git repo #{url}"
+    exec_local "cd #{git_cache_directory} && git clone #{url} #{name}"
+  end
+  exec_local "cd #{target} && git pull > /dev/null"
+  cookbook = File.join(target, "cookbooks")
+  cookbooks << cookbook if File.exists? cookbook
+  role = File.join(target, "roles")
+  roles << role if File.exists? role
+end
+
+puts cookbooks.inspect
+puts roles.inspect
 
 log_level :info
 log_location STDOUT
 json_attribs json_file.path
-cookbook_path config["repos"]
-role_path config["repos"].first + "/roles"
+cookbook_path cookbooks
+role_path roles
