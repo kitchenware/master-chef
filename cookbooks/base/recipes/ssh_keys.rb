8fcc36beeaa207bc3b542697af07c82b7992babe
Bertrand Paquet <bpaquet@octo.com>
2012-02-27 21:47:19 +0100

    Refactor ssh keys

--- cookbooks/base/recipes/ssh_keys.rb
+++ cookbooks/base/recipes/ssh_keys.rb
@@ -1,23 +1,37 @@
 
 if node[:ssh_keys]
 
-  node.ssh_keys.each do |keys|
+  node[:resolved_ssh_keys] = {}
 
-    keys["users"].each do |user|
-      home = get_home user
+  node.ssh_keys.each do |name, config|
 
-      directory "#{home}/.ssh" do
-        owner user
-        mode 0700
+    if config["users"]
+      config["users"].each do |user|
+        node.resolved_ssh_keys[user] = [] unless node.resolved_ssh_keys[user]
+        node.resolved_ssh_keys[user] += config["keys"] if config["keys"]
+        if config["ref"]
+          config["ref"].each do |k|
+            node.resolved_ssh_keys[user] += node.ssh_keys[k]["keys"]
+          end
+        end
       end
+    end
 
-      template "#{home}/.ssh/authorized_keys" do
-        owner user
-        mode 0700
-        variables :keys => keys["keys"]
-        source "authorized_keys.erb"
-      end
+  end
 
+  node.resolved_ssh_keys.each do |user, keys|
+    home = get_home user
+
+    directory "#{home}/.ssh" do
+      owner user
+      mode 0700
+    end
+
+    template "#{home}/.ssh/authorized_keys" do
+      owner user
+      mode 0700
+      variables :keys => keys.uniq.sort
+      source "authorized_keys.erb"
     end
 
   end
