f312044b878e6c9c4650c060c2d8e30982d06f6c
Bertrand Paquet <bpaquet@octo.com>
2012-02-27 09:57:16 +0100

    Add vmware tools script

---
+++ cookbooks/vmware_tools/recipes/default.rb
@@ -0,0 +1,9 @@
+
+cookbook_file "/etc/init.d/vmware-tools-update" do
+  source "vmware-tools-update"
+  mode 0755
+end
+
+link "/etc/rcS.d/S36vmware-tools-update" do
+  to "/etc/init.d/vmware-tools-update"
+end
