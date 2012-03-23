f1b5b0c5897d4f103203f022f8e0f2572b7fb3f3
Mikael Robert <mikaelrob@gmail.com>
2012-03-23 12:04:21 +0100

    changed mod prefork conf

--- cookbooks/apache2/attributes/default.rb
+++ cookbooks/apache2/attributes/default.rb
@@ -1,9 +1,10 @@
 default[:apache2][:mpm] = :prefork
 
 default[:apache2][:mpm_config][:prefork] = {
-  :start => 5,
-  :min_spare => 5,
-  :max_spare => 10,
-  :max_clients => 150,
+  :start => 10,
+  :min_spare => 10,
+  :max_spare => 20,
+  :server_limit => 256,
+  :max_clients => 256,
   :max_request_per_child => 20,
 }
