51497e06d676addb193f125b4aba1a3d92d17202
Bertrand Paquet <bpaquet@octo.com>
2012-04-05 11:03:39 +0200

    Update exim recipe

--- cookbooks/exim4/recipes/only_send.rb
+++ cookbooks/exim4/recipes/only_send.rb
@@ -2,6 +2,6 @@
 include_recipe "exim4::base"
 
 template "/etc/exim4/update-exim4.conf.conf" do
-  source "smart_host.update-exim4.conf.erb"
+  source "only_send.update-exim4.conf.erb"
   notifies :restart, resources(:service => "exim4")
 end
