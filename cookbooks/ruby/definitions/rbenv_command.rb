ec97c543a3687c2a1b85f90656ba6c2aff7894d6
Bertrand Paquet <bpaquet@octo.com>
2012-04-16 12:14:23 +0200

    Fix rbenv_command initialisation

--- cookbooks/ruby/definitions/rbenv_command.rb
+++ cookbooks/ruby/definitions/rbenv_command.rb
@@ -14,12 +14,10 @@ define :ruby_rbenv_command, {
   bash "rbenv command : #{ruby_rbenv_command_params[:name]}" do
     user ruby_rbenv_command_params[:user]
     cmd = []
-    cmd << "unset RBENV_DIR"
-    cmd << "unset RBENV_HOOK_PATH"
-    cmd << "unset RBENV_ROOT"
     cmd << "export HOME=#{get_home ruby_rbenv_command_params[:user]}"
     cmd << "export PATH=$HOME/.rbenv/bin:$PATH"
-    cmd << "eval \"$(rbenv init -)\""
+    cmd << "cd $HOME"
+    cmd << "source .warp/common/ruby/include"
     cmd << "cd #{ruby_rbenv_command_params[:directory]}"
     cmd << "#{ruby_rbenv_command_params[:code]}"
     if ruby_rbenv_command_params[:file_check]
@@ -27,7 +25,6 @@ define :ruby_rbenv_command, {
       cmd << "touch #{ruby_rbenv_command_params[:file_check]}"
     end
     code cmd.join(" && ")
-    
   end
 
 end
