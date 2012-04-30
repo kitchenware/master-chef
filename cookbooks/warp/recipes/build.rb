09a1a198c12d6b85e888bc8b110c2a05ad31c9a5
Bertrand Paquet <bpaquet@octo.com>
2012-04-30 17:53:23 +0200

    Improve warp::build

--- cookbooks/warp/recipes/build.rb
+++ cookbooks/warp/recipes/build.rb
@@ -3,6 +3,12 @@ package "s3cmd"
 
 package "build-essential"
 
+include_recipe "java"
+
 ruby_user "build" do
   install_rbenv true
+end
+
+sudo_sudoers_file "build" do
+  content "build ALL=(ALL) NOPASSWD:ALL"
 end
