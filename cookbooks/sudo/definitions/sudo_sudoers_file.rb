7d496474ebbabf2bfc465aaf833b1a555bb1a7a5
Bertrand Paquet <bpaquet@octo.com>
2012-05-01 09:57:19 +0200

    Fix #10 : add includedir in sudoers file if needed

--- cookbooks/sudo/definitions/sudo_sudoers_file.rb
+++ cookbooks/sudo/definitions/sudo_sudoers_file.rb
@@ -7,6 +7,16 @@ define :sudo_sudoers_file, {
 
   raise "Please specify content with sudo_sudoers_file" unless sudo_sudoers_file_params[:content]
 
+  directory "/etc/sudoers.d" do
+    owner "root"
+    mode 0755
+  end
+
+  execute "add includedir /etc/sudoers.d in sudoers" do
+    command "echo '#includedir /etc/sudoers.d' >> /etc/sudoers"
+    not_if "cat /etc/sudoers | grep '#includedir /etc/sudoers.d'"
+  end
+
   template "/etc/sudoers.d/#{sudo_sudoers_file_params[:name]}" do
     cookbook "sudo"
     source "sudoers.erb"
