0a8e8a8ea55fb7cd713b8fe499ac25c24c1768c6
Bertrand Paquet <bpaquet@octo.com>
2012-05-01 22:04:05 +0200

    Improve munin local recipe

--- cookbooks/munin/recipes/local.rb
+++ cookbooks/munin/recipes/local.rb
@@ -1,2 +1,18 @@
 
+include_recipe "cron"
+
 package "munin"
+
+execute "remove munin-cron" do
+  command "sed -i -e '/munin-cron/d' /etc/cron.d/munin"
+  only_if "grep munin-cron /etc/cron.d/munin"
+end
+
+template "/etc/munin/graph.sh" do
+  source "graph.sh.erb"
+  mode 0755
+end
+
+cron_file "munin-update" do
+  content "*/5 * * * *     munin if [ -x /usr/share/munin/munin-update ]; then /usr/share/munin/munin-update; fi"
+end
