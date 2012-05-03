f1984ecccbf4e40688dd02985f9a10f2a271e27f
Bertrand Paquet <bpaquet@octo.com>
2012-05-03 10:50:38 +0200

    Add upload script for warp:build

--- cookbooks/warp/recipes/build.rb
+++ cookbooks/warp/recipes/build.rb
@@ -12,3 +12,10 @@ end
 sudo_sudoers_file "build" do
   content "build ALL=(ALL) NOPASSWD:ALL"
 end
+
+template "#{get_home "build"}/upload.sh" do
+  owner "build"
+  mode 0755
+  source "upload.sh.erb"
+  variables :bucket => "warp-repo"
+end
