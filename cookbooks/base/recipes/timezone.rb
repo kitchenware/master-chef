5dd7440a37c6040df0e1b07dd03ab40716aae4d6
Bertrand Paquet <bpaquet@octo.com>
2012-05-04 15:51:36 +0200

    Fix #13 : timezone was not reconfigured correctly

--- cookbooks/base/recipes/timezone.rb
+++ cookbooks/base/recipes/timezone.rb
@@ -1,10 +1,23 @@
 
-link "/etc/localtime" do
-  to "/usr/share/zoneinfo/UTC"
+if node.timezone =~ /^([^\/]+)\/([^\/]+)$/
+
+  zone, city = $1, $2
+
+  target = "/usr/share/zoneinfo/#{zone}/#{city}"
+
+  raise "No locatime file found for #{node.timezone}" unless File.exist? target
+
+  link "/etc/localtime" do
+    to target
+  end
+
+  template "/etc/timezone" do
+    source "timezone.erb"
+    mode 0644
+    variables :timezone => node.timezone
+  end
+
+else 
+  raise "Wrong timezone #{node.timezone}"
 end
 
-template "/etc/timezone" do
-  source "timezone.erb"
-  mode 0644
-  variables :timezone => node.timezone
-end
