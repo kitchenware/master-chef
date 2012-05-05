9c1a17b4ab13530dc9f74fda23193acee688656e
Bertrand Paquet <bpaquet@octo.com>
2012-05-05 14:45:09 +0200

    Change way of setting mysql root password to avoid execution order problems

--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -1,12 +1,14 @@
 package "mysql-server"
 
-local_storage_read("mysql_password:root") do
+root_mysql_password = local_storage_read("mysql_password:root") do
   password = PasswordGenerator.generate 32
   Chef::Log.info "Mysql : new root password generated"
-  execute "change mysql root password" do
-    command "mysqladmin -u root password #{password}"
-  end
   password
+end
+
+execute "change mysql root password" do
+  command "mysqladmin -u root password #{root_mysql_password}"
+  only_if "echo 'select 1;' | mysql --user=root --password= "
 end
 
 if node[:mysql] && node.mysql[:databases]
