596f1eb1fe7eb4176ff5e4daad2a5d4c190f9f50
Bertrand Paquet <bpaquet@octo.com>
2012-05-07 16:29:21 +0200

    Deploy jdk via warp

---
+++ cookbooks/java/definitions/default.rb
@@ -0,0 +1,24 @@
+
+
+define :java_from_warp, {
+
+} do
+
+  jdk = node.java.versions[params[:name]]
+
+  warp_file = "#{jdk}_`arch`.warp"
+
+  bash "install #{jdk}" do
+    code "cd /tmp && wget #{node.warp.warp_src}/#{warp_file} && sh #{warp_file} && rm #{warp_file}"
+    not_if "[ -d /usr/lib/jvm/#{jdk} ]"
+  end
+
+  bash "update alternative #{jdk}" do
+    code <<-EOF
+    update-alternatives --install /usr/bin/java java /usr/lib/jvm/#{jdk}/bin/java 1 &&
+    update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/#{jdk}/bin/javac 1
+    EOF
+    not_if "update-alternatives --list java | grep #{jdk}"
+  end
+
+end
--- cookbooks/java/recipes/java_sun_java6.rb
+++ cookbooks/java/recipes/java_sun_java6.rb
@@ -1,20 +1,2 @@
 
-  if node['platform'] == "ubuntu"
-    base_ppa "sun-java" do
-      url "ppa:sun-java-community-team/sun-java6"
-    end
-  end
-
-  if node['platform'] == "debian"
-    add_apt_repository "sun-java" do
-       url "http://ftp.ie.debian.org/debian"
-       components ["non-free"]
-    end
-  end
-    
-  bash "auto accept java license" do
-    code "echo sun-java6-jdk shared/accepted-sun-dlj-v1-1 select true | /usr/bin/debconf-set-selections"
-    not_if "dpkg -s sun-java6-jdk"
-  end
-
-  package "sun-java6-jdk"
+java_from_warp "sun_java6"
