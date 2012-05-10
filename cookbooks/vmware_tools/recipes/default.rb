61171803892ac9fc8be345d64d8c2d56ad702693
Bertrand Paquet <bpaquet@octo.com>
2012-05-10 11:44:26 +0200

    Improve vmware-tools for kernel 3.0

--- cookbooks/vmware_tools/recipes/default.rb
+++ cookbooks/vmware_tools/recipes/default.rb
@@ -1,6 +1,11 @@
 
+
+vmware_config_options = ""
+vmware_config_options = "--clobber-kernel-modules=vmxnet3,pvscsi,vmmemctl" if %x{uname -r} =~ /^3.*$/
+
 template "/etc/init.d/vmware-tools-update" do
   source "vmware-tools-update"
+  variables :vmware_config_options => vmware_config_options
   mode 0755
 end
 
