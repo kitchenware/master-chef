e261cbafb3437f4d1844a339e3b0dbfa8a45a894
Mikael Robert <mikaelrob@gmail.com>
2012-05-14 18:00:29 +0200

    fixed nexus deployment with new tomcat directive
    Signed-off-by: Mikael Robert <mikaelrob@gmail.com>

--- cookbooks/nexus/attributes/default.rb
+++ cookbooks/nexus/attributes/default.rb
@@ -1,2 +1,10 @@
 default[:nexus][:location] = "/nexus"
-default[:nexus][:war_url] = "http://www.sonatype.org/downloads/nexus-2.0.4.war"
+default[:nexus][:version] = "2.0.4"
+default[:nexus][:war_url] = "http://www.sonatype.org/downloads/nexus-#{default[:nexus][:version]}.war"
+default[:nexus][:path][:build] = "/home/chef"
+default[:nexus][:tomcat] = {
+  :name => 'nexus',
+  :env => {
+    'JAVA_OPTS' => '-XX:MaxPermSize=256m -Xmx1024m -Xms256m',
+  }
+}
