86ca50c5e7ceaa6d18f368fda4dc0d51b29262b0
Bertrand Paquet <bpaquet@octo.com>
2012-08-21 17:26:04 +0200

    Trailing spaces

--- cookbooks/rails/recipes/default.rb
+++ cookbooks/rails/recipes/default.rb
@@ -2,7 +2,7 @@
 if node[:rails_app]
 
   node.rails_app.each do |k, v|
-    
+
     unicorn_rails_app k do
       user v[:user]
       app_directory v[:app_directory] if v[:app_directory]
@@ -12,7 +12,7 @@ if node[:rails_app]
       code_for_initd v[:code_for_initd] if v[:code_for_initd]
       configure_nginx v[:configure_nginx] if v[:configure_nginx]
     end
-  
+
   end
 
 end
