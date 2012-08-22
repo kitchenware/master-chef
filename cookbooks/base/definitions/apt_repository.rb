f6327c59732965261e62376c07953c0f59e4f315
Bertrand Paquet <bpaquet@octo.com>
2012-08-22 14:10:15 +0200

    Trailing spaces

--- cookbooks/base/definitions/apt_repository.rb
+++ cookbooks/base/definitions/apt_repository.rb
@@ -15,7 +15,7 @@ define :add_apt_repository, {
   template "/etc/apt/sources.list.d/#{add_apt_repository_params[:name]}.list" do
     cookbook "base"
     variables :distrib => %x{lsb_release -cs}.strip, :components => add_apt_repository_params[:components], :url => add_apt_repository_params[:url]
-    source "repository.erb" 
+    source "repository.erb"
     mode 0644
     notifies :run, "bash[apt-get-update]", :immediately
   end
