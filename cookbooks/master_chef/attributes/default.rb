c2bdf7ca3d30d70f71c7531d6a2de1f8e4568ac4
Bertrand Paquet <bpaquet@octo.com>
2012-08-25 10:19:53 +0200

    Allow to not load config from MASTER_CHEF_CONFIG

--- cookbooks/master_chef/attributes/default.rb
+++ cookbooks/master_chef/attributes/default.rb
@@ -1,16 +1,21 @@
-config_file = ENV['MASTER_CHEF_CONFIG']
 
-raise "Please specify config file with env var MASTER_CHEF_CONFIG" unless config_file
-raise "File not found #{config_file}" unless File.exists? config_file
+unless ENV['NO_MASTER_CHEF_CONFIG']
 
-config = JSON.load(File.read(config_file))
+  config_file = ENV['MASTER_CHEF_CONFIG']
 
-node_config = config["node_config"]
+  raise "Please specify config file with env var MASTER_CHEF_CONFIG" unless config_file
+  raise "File not found #{config_file}" unless File.exists? config_file
 
-if node_config
-  node_config.each do |k, v|
-    normal[k] = v
+  config = JSON.load(File.read(config_file))
+
+  node_config = config["node_config"]
+
+  if node_config
+    node_config.each do |k, v|
+      normal[k] = v
+    end
   end
+
 end
 
 default[:master_chef][:chef_solo_scripts] = {
