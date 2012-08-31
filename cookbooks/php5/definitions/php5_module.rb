90aa47ec1503347d0fec5fcbdfc8003d0dc9f7e4
Bertrand Paquet <bpaquet@octo.com>
2012-08-31 12:21:53 +0200

    Add restart on php5 module install for php5-fpm

--- cookbooks/php5/definitions/php5_module.rb
+++ cookbooks/php5/definitions/php5_module.rb
@@ -6,6 +6,7 @@ define :php5_module, {
 
   package "php5-#{php5_module_params[:name]}" do
     notifies :reload, resources(:service => "apache2") if find_resources_by_name_pattern(/^apache2$/).size > 0
+    notifies :reload, resources(:service => "php5-fpm") if find_resources_by_name_pattern(/^php5-fpm$/).size > 0
   end
 
 end
