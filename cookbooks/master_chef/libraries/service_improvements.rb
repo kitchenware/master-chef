975c36cf6358f9b909b9aa804d84ccffba29166f
Bertrand Paquet <bpaquet@octo.com>
2012-09-06 00:05:51 +0200

    More fixing on service starting / restarting

--- cookbooks/master_chef/libraries/service_improvements.rb
+++ cookbooks/master_chef/libraries/service_improvements.rb
@@ -23,7 +23,13 @@ module ServiceHelper
   @@service_before_start = Dir.entries('/etc/init.d').reject{|x| x == '.' || x == '..'}
 
   def auto_compute_action
-    @@service_before_start.include?(self.name) ? [:enable, :delayed_restart] : [:enable, :stop, :delayed_restart]
+    # why stop on new service : because the default configuration of a service can lock some resources
+    # like port 80 and can break start of another service, which want to access to this resource
+    # and which is not yet restarted
+    # why restart on new service (and not just delayed start) : because if service is new,
+    # it's probable that some config files will be deployed, and a restart command will be issued
+    # some services like tomcat does not support very well to be started and immediately after restarted
+    @@service_before_start.include?(self.name) ? [:enable, :start] : [:enable, :stop, :delayed_restart]
   end
 
 end
