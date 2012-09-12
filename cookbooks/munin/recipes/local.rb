47bd6bfdc8bedd3ec83a37724f2a5120f5062247
Mikael Robert <mikaelrob@gmail.com>
2012-09-12 15:26:01 +0200

    added new line at end of cron for munin

--- cookbooks/munin/recipes/local.rb
+++ cookbooks/munin/recipes/local.rb
@@ -14,5 +14,5 @@ template "/etc/munin/graph.sh" do
 end
 
 cron_file "munin-update" do
-  content "*/5 * * * *     munin if [ -x /usr/share/munin/munin-update ]; then /usr/share/munin/munin-update; fi"
+  content "*/5 * * * *     munin if [ -x /usr/share/munin/munin-update ]; then /usr/share/munin/munin-update; fi \n\n"
 end
