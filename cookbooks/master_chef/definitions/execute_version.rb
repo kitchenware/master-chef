1c27e1cd26aee4528587a1eac09e555cd9405f38
Bertrand Paquet <bpaquet@octo.com>
2012-09-20 20:37:08 +0200

    Fix naming in execute_version

--- cookbooks/master_chef/definitions/execute_version.rb
+++ cookbooks/master_chef/definitions/execute_version.rb
@@ -6,18 +6,18 @@ define :execute_version, {
   :notifies => nil,
 } do
 
-  execute_install_params = params
+  execute_version_params = params
 
-  raise "Please specify command with execute_install" unless execute_install_params[:command]
+  raise "Please specify command with execute_version" unless execute_version_params[:command]
 
   directory node.local_storage.version_storage
 
-  installed_file = "#{node.local_storage.version_storage}/#{execute_install_params[:name].gsub(/ /, '_')}"
+  installed_file = "#{node.local_storage.version_storage}/#{execute_version_params[:name].gsub(/ /, '_')}"
 
-  execute "install #{execute_install_params[:name]}" do
-    command "rm -f #{installed_file} && su #{execute_install_params[:user]} -c '#{execute_install_params[:command]}' && echo #{execute_install_params[:version]} > #{installed_file}"
-    not_if "[ -f #{installed_file} ] && [ \"`cat #{installed_file}`\" = \"#{execute_install_params[:version]}\" ]"
-    notifies execute_install_params[:notifies][0], execute_install_params[:notifies][1] if execute_install_params[:notifies]
+  execute "install #{execute_version_params[:name]}" do
+    command "rm -f #{installed_file} && su #{execute_version_params[:user]} -c '#{execute_version_params[:command]}' && echo #{execute_version_params[:version]} > #{installed_file}"
+    not_if "[ -f #{installed_file} ] && [ \"`cat #{installed_file}`\" = \"#{execute_version_params[:version]}\" ]"
+    notifies execute_version_params[:notifies][0], execute_version_params[:notifies][1] if execute_version_params[:notifies]
   end
 
 end
