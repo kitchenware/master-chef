1726945266da5397a2403f851c9998a54a325745
Bertrand Paquet <bpaquet@octo.com>
2012-10-23 20:23:06 +0200

    Add error handler to exim4

--- cookbooks/exim4/recipes/base.rb
+++ cookbooks/exim4/recipes/base.rb
@@ -5,3 +5,5 @@ service "exim4" do
   supports :status => true, :reload => true, :restart => true
   action auto_compute_action
 end
+
+Chef::Config.exception_handlers << ServiceErrorHandler.new("exim4", "exim")
