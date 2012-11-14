0120b18cd84761a06a721b8c476106e60ec58db8
Bertrand Paquet <bpaquet@octo.com>
2012-11-14 12:57:43 +0100

    Improve exim4 config

---
+++ cookbooks/exim4/attributes/default.rb
@@ -0,0 +1,4 @@
+default[:exim4] = {
+  :interfaces => ['127.0.0.1'],
+  :relay_nets => [],
+}
--- cookbooks/exim4/recipes/smarthost.rb
+++ cookbooks/exim4/recipes/smarthost.rb
@@ -5,3 +5,21 @@ template "/etc/exim4/update-exim4.conf.conf" do
   source "smart_host.update-exim4.conf.erb"
   notifies :restart, resources(:service => "exim4")
 end
+
+if node.exim4.smarthost =~ /localhost/
+
+  bash "add self = send to exim4.conf.template" do
+    code "sed -i '/^smarthost/ a   self=send' /etc/exim4/exim4.conf.template "
+    not_if "cat /etc/exim4/exim4.conf.template  | grep -A5 '^smarthost:' | grep self"
+    notifies :restart, resources(:service => "exim4")
+  end
+
+else
+
+  bash "remove self = send to exim4.conf.template" do
+    code "sed -i '/^self/d' /etc/exim4/exim4.conf.template "
+    only_if "cat /etc/exim4/exim4.conf.template  | grep -A5 '^smarthost:' | grep self"
+    notifies :restart, resources(:service => "exim4")
+  end
+
+end
