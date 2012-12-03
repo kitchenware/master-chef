24b7bad4c0bbd18195ae123a38ce7728b6a1b89e
Bertrand Paquet <bpaquet@octo.com>
2012-12-03 22:31:22 +0100

    Fix supervisor restart : modify init_d and add a restart delay related to number of jobs

--- cookbooks/supervisor/attributes/default.rb
+++ cookbooks/supervisor/attributes/default.rb
@@ -1,1 +1,2 @@
 default[:supervisor][:log_dir] = "/var/log/supervisor"
+default[:supervisor][:restart_delay_by_job] = 4
--- cookbooks/supervisor/recipes/default.rb
+++ cookbooks/supervisor/recipes/default.rb
@@ -3,6 +3,23 @@ include_recipe "sudo"
 
 package "supervisor"
 
+# http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=609457
+if node.platform == "debian" || node.platform == "ubuntu"
+
+  template "/etc/init.d/supervisor" do
+    source "init_d"
+    mode 0755
+  end
+
+end
+
+template "/etc/default/supervisor" do
+  source "supervisor.erb"
+  variables :dodtime => Proc.new { find_resources_by_name_pattern(/^\/etc\/supervisor\/conf.d\/.*\.conf$/).length * node.supervisor.restart_delay_by_job }
+  mode 0644
+end
+
+
 service "supervisor" do
   supports :status => true
   action auto_compute_action
