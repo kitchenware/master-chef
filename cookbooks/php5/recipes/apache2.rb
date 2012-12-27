ee90f948819df1b43f86e5df6da7329742f52831
Bertrand Paquet <bpaquet@octo.com>
2012-12-27 20:02:45 +0100

    Simplify php5

--- cookbooks/php5/recipes/apache2.rb
+++ cookbooks/php5/recipes/apache2.rb
@@ -2,14 +2,40 @@
 include_recipe "apache2"
 include_recipe "php5"
 
+package "libapache2-mod-php5" do
+  notifies :reload, resources(:service => "apache2")
+end
+
+template "/etc/php5/apache2/php.ini" do
+  mode '0644'
+  cookbook "php5"
+  source "php5.ini.erb"
+  variables node.php5.php_ini
+  notifies :reload, resources(:service => "apache2")
+end
+
+apache2_enable_module "setenvif"
+
+file "/etc/apache2/conf.d/https_php" do
+  content "SetEnvIf X-Forwarded-Proto https HTTPS=on"
+  mode '0644'
+  notifies :reload, resources(:service => "apache2")
+end
+
+if node.php5.php_ini["error_log"]
+
+  directory File.dirname(node.php5.php_ini["error_log"]) do
+    owner "www-data"
+    mode '0755'
+    recursive true
+  end
+
+end
+
 if node.apache2.default_vhost.enable
   
   apache2_vhost "apache2:default_vhost" do
     options :cookbook => "apache2"
   end
   
-end
-
-php5_apache2 "php5" do
-  options node.php5[:apache2]
 end
