6cc1014fba3dbdd675b2fdfd64b98e0ffd6fb6b6
Bertrand Paquet <bpaquet@octo.com>
2013-01-12 12:43:56 +0100

    Remove debug

--- cookbooks/php5/definitions/php5_pear_module.rb
+++ cookbooks/php5/definitions/php5_pear_module.rb
@@ -9,7 +9,7 @@ define :php5_pear_module, {
   opts << "--alldeps" if php5_pear_module_params[:alldeps]
 
   execute "install pear module #{php5_pear_module_params[:name]}" do
-    command "pear install #{opts.join(' ')} #{php5_pear_module_params[:name]} > /tmp/toto"
+    command "pear install #{opts.join(' ')} #{php5_pear_module_params[:name]}"
     not_if "pear list-files #{php5_pear_module_params[:name]} > /dev/null"
   end
 
