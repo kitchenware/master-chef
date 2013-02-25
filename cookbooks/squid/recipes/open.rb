8ac7ab9ea33cb8131f2a48c9784eb4eab2fa388f
Bertrand Paquet <bpaquet@octo.com>
2013-02-25 22:54:57 +0100

    Allow git clone via squid

--- cookbooks/squid/recipes/open.rb
+++ cookbooks/squid/recipes/open.rb
@@ -12,3 +12,9 @@ execute "remove via on on squid config" do
   not_if "grep 'via off' /etc/squid3/squid.conf"
   notifies :restart, resources(:service => "squid3")
 end
+
+execute "add ignore_expect_100 on on squid config" do
+  command "sed -ie 's/# ignore_expect_100 off/ignore_expect_100 on/' /etc/squid3/squid.conf"
+  not_if "grep 'ignore_expect_100 on' /etc/squid3/squid.conf"
+  notifies :restart, resources(:service => "squid3")
+end
