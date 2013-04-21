538545b51c7c91f4f33e307260e2516589e14610
Bertrand Paquet <bpaquet@octo.com>
2013-04-21 11:32:59 +0200

    Avoid user conflict for accept_ssh_host_key

--- cookbooks/base/definitions/ssh_accept_host_key.rb
+++ cookbooks/base/definitions/ssh_accept_host_key.rb
@@ -6,7 +6,7 @@ define :ssh_accept_host_key, {
 
   raise "Please specify user with ssh_accept_host_key" unless ssh_accept_host_key_params[:user]
 
-  execute "accept key for #{ssh_accept_host_key_params[:name]}" do
+  execute "accept key for #{ssh_accept_host_key_params[:name]} for user #{ssh_accept_host_key_params[:user]}" do
     command "ssh -o StrictHostKeyChecking=no #{ssh_accept_host_key_params[:name]} exit || true"
     user ssh_accept_host_key_params[:user]
     not_if "ssh-keygen -F #{ssh_accept_host_key_params[:name]} -f #{get_home ssh_accept_host_key_params[:user]}/.ssh/known_hosts"
