53c756b1be5b4d3d3958d66fad0fb28f34b16f8d
Bertrand Paquet <bpaquet@octo.com>
2013-04-24 14:57:23 +0200

    Manage port in ssh_accept_keys

--- cookbooks/base/definitions/ssh_accept_host_key.rb
+++ cookbooks/base/definitions/ssh_accept_host_key.rb
@@ -1,15 +1,28 @@
 
 define :ssh_accept_host_key, {
   :user => nil,
+  :port => 22,
 } do
+
   ssh_accept_host_key_params = params
 
   raise "Please specify user with ssh_accept_host_key" unless ssh_accept_host_key_params[:user]
 
-  execute "accept key for #{ssh_accept_host_key_params[:name]} for user #{ssh_accept_host_key_params[:user]}" do
-    command "ssh -o StrictHostKeyChecking=no #{ssh_accept_host_key_params[:name]} exit || true"
+  host = ssh_accept_host_key_params[:name]
+  host = host.split('@')[1] if host.match /@/
+
+  ssh_keygen_host = host
+
+  ssh_opts = "-o StrictHostKeyChecking=no "
+  if ssh_accept_host_key_params[:port].to_s != "22"
+    ssh_opts += "-p #{ssh_accept_host_key_params[:port]} "
+    ssh_keygen_host = "[#{host}]:#{ssh_accept_host_key_params[:port]}"
+  end
+
+  execute "accept key for #{host} for user #{ssh_accept_host_key_params[:user]} port #{ssh_accept_host_key_params[:port]}" do
+    command "ssh #{ssh_opts} #{host} exit || true"
     user ssh_accept_host_key_params[:user]
-    not_if "ssh-keygen -F #{ssh_accept_host_key_params[:name]} -f #{get_home ssh_accept_host_key_params[:user]}/.ssh/known_hosts"
+    not_if "ssh-keygen -F #{ssh_keygen_host} -f #{get_home ssh_accept_host_key_params[:user]}/.ssh/known_hosts | grep ."
   end
 
 end
