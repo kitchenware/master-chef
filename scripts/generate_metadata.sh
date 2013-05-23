1ce83b172be4343d83f96afacce90518d2d717b9
Bertrand Paquet <bpaquet@octo.com>
2013-05-23 17:20:54 +0200

    Fix detection for ' in generate_metadata

--- scripts/generate_metadata.sh
+++ scripts/generate_metadata.sh
@@ -2,10 +2,10 @@
 
 for dir in $@; do
   for i in `ls $dir`; do
-    echo "Processing $i"
+    echo "Processing $dir/$i"
     echo "name '$i'" > $dir/$i/metadata.rb
     echo "" >> $dir/$i/metadata.rb
-    (grep -r "include_recipe" $dir/$i | perl -pe 's/^.*include_recipe ["''](.*)["''].*$/\1/g' | perl -pe 's/(.*)::.*/\1/g' | grep -v $i | sort | uniq | while read j; do
+    (grep -r "include_recipe" $dir/$i | perl -pe 's/^.*include_recipe ["'\''](.*)["'\''].*$/\1/g' | perl -pe 's/(.*)::.*/\1/g' | grep -v $i | sort | uniq | while read j; do
         echo "depends :$j"
     done) >> $dir/$i/metadata.rb
   done
