2231095dda824bde9d105aa8bd468e906967026b
Bertrand Paquet <bpaquet@octo.com>
2013-05-29 08:38:57 +0200

    One more fix on ssh_user_dir

--- cookbooks/base/definitions/ssh_user_dir.rb
+++ cookbooks/base/definitions/ssh_user_dir.rb
@@ -7,13 +7,15 @@ define :ssh_user_directory, {
 
   dir = "#{get_home ssh_user_directory_params[:name]}/.ssh"
 
-  if find_resources_by_name(dir).empty?
+  r = find_resources_by_name(dir)
+  r.first.run_action :create if r.length == 1 && r.first.action.length > 0 && r.first.action.first == :nothing
+
+  if r.empty?
 
     directory dir do
       mode '0700'
       owner ssh_user_directory_params[:name]
       action ssh_user_directory_params[:action]
-
     end
 
     dir
