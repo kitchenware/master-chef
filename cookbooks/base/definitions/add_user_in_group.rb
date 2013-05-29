98f39e2f4d0c2e6ccccd16318ebf94afd7089ab4
Bertrand Paquet <bpaquet@octo.com>
2013-05-29 14:29:28 +0200

    Fix add_user_in_group

--- cookbooks/base/definitions/add_user_in_group.rb
+++ cookbooks/base/definitions/add_user_in_group.rb
@@ -3,13 +3,13 @@ define :add_user_in_group, {
   :group => nil,
   } do
 
-  add_user_in_group = params
+  add_user_in_group_params = params
 
-  raise "Please specify group for add_user_in_group" unless add_user_in_group[:group]
+  raise "Please specify group for add_user_in_group" unless add_user_in_group_params[:group]
 
-  group add_user_in_group[:group] do
+  group add_user_in_group_params[:group] do
     action :manage
-    members [add_user_in_group[:name]]
+    members [add_user_in_group_params[:name]]
     append true
   end
 
