b642a2bc9968f87c41f94d5945e6901259157deb
Bertrand Paquet <bpaquet@octo.com>
2013-05-30 16:37:28 +0200

    Fix package holding

--- cookbooks/base/definitions/hold_package_version.rb
+++ cookbooks/base/definitions/hold_package_version.rb
@@ -3,9 +3,25 @@ define :hold_package_version, {
 } do
   hold_package_version_params = params
 
-  execute "fix #{hold_package_version_params[:name]} version" do
+  execute "hold #{hold_package_version_params[:name]} version" do
     command "echo #{hold_package_version_params[:name]} hold | dpkg --set-selections"
     not_if "dpkg --get-selections | grep #{hold_package_version_params[:name]} | grep -q hold"
   end
+
+end
+
+define :package_fixed_version, {
+  :version => nil,
+} do
+  package_fixed_version_params = params
+
+  raise "Please specify version with package_fixed_version" unless package_fixed_version_params[:version]
+
+  package package_fixed_version_params[:name] do
+    version package_fixed_version_params[:version]
+    options "--force-yes"
+  end
+
+  hold_package_version package_fixed_version_params[:name]
 
 end
