b50f969a01fa0f3ff083b5cb52d677456d6f91eb
mikrob <mrobert@octo.com>
2013-06-07 15:10:33 +0200

    added script to manage logrotate file with json

--- cookbooks/logrotate/recipes/default.rb
+++ cookbooks/logrotate/recipes/default.rb
@@ -13,3 +13,17 @@ delayed_exec "Remove useless logrotate files" do
     end
   end
 end
+
+if node[:logrotate]
+  node.logrotate.each do |k, v|
+   logrotate_file k do
+     files v
+   end
+ end
+end
+
+delayed_exec "Remove useless logrotate files" do
+  block do
+    files = find_resources_by_name_pattern(/^\/etc\/logrotate.d\/.*$/).map{|r| r.name}
+  end
+end
