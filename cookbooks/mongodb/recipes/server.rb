f43c68ada88c2b05b30c40553b0f9998fe7097d9
mikrob <mrobert@octo.com>
2013-06-10 11:53:50 +0200

    refactor mongodb repo adding

--- cookbooks/mongodb/recipes/server.rb
+++ cookbooks/mongodb/recipes/server.rb
@@ -1,22 +1,12 @@
-if node['platform'] == "ubuntu"
+if node['platform'] == "ubuntu" && node.apt.master_chef_add_apt_repo
 
 	add_apt_repository "ubuntu_10gen" do
 		url "http://downloads-distro.mongodb.org/repo/ubuntu-upstart"
 		distrib "dist"
 		components ['10gen']
-		run_apt_get_update false
-	end
-	
-	directory "/etc/apt/keys"
-
-	template "/etc/apt/keys/ubuntu_10gen" do
-		source "apt_keys/ubuntu_10gen"
-		mode '0644'
-	end
-
-	execute "accept key for ubuntu_10gen" do
-		command "apt-key add /etc/apt/keys/ubuntu_10gen"
-		not_if "apt-key list | grep 7F0CEB10"
+		key "7F0CEB10"
+		key_server "keyserver.ubuntu.com"
+		run_apt_get_update true
 	end
 end
 
