11599ac85bb1ebdf8d363efcbb4e509a0e034591
Bertrand Paquet <bpaquet@octo.com>
2013-06-10 13:26:29 +0200

    Add mongodb config for debian

--- cookbooks/mongodb/recipes/server.rb
+++ cookbooks/mongodb/recipes/server.rb
@@ -10,6 +10,18 @@ if node['platform'] == "ubuntu" && node.apt.master_chef_add_apt_repo
 	end
 end
 
+if node['platform'] == "debian" && node.apt.master_chef_add_apt_repo
+
+	add_apt_repository "ubuntu_10gen" do
+		url "http://downloads-distro.mongodb.org/repo/debian-sysvinit"
+		distrib "dist"
+		components ['10gen']
+		key "7F0CEB10"
+		key_server "keyserver.ubuntu.com"
+		run_apt_get_update true
+	end
+end
+
 package_fixed_version "mongodb-10gen" do
 	version node.mongodb.version
 end
