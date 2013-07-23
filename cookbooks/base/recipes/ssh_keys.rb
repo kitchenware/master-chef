0d380a59cee4ef32537c46ab51660aae42032786
Bertrand Paquet <bpaquet@octo.com>
2013-07-23 16:25:36 +0200

    Allow to disable ssh_keys by config

--- cookbooks/base/recipes/ssh_keys.rb
+++ cookbooks/base/recipes/ssh_keys.rb
@@ -6,12 +6,14 @@ if node[:ssh_keys]
   node.ssh_keys.each do |name, config|
 
     if config["users"]
-      config["users"].each do |user|
-        node.set[:resolved_ssh_keys][user] = [] unless node.resolved_ssh_keys[user]
-        node.set[:resolved_ssh_keys][user] = node.resolved_ssh_keys[user] + config["keys"] if config["keys"]
-        if config["ref"]
-          config["ref"].each do |k|
-            node.set[:resolved_ssh_keys][user] = node.resolved_ssh_keys[user] + node.ssh_keys[k]["keys"]
+      unless config["disabled"]
+        config["users"].each do |user|
+          node.set[:resolved_ssh_keys][user] = [] unless node.resolved_ssh_keys[user]
+          node.set[:resolved_ssh_keys][user] = node.resolved_ssh_keys[user] + config["keys"] if config["keys"]
+          if config["ref"]
+            config["ref"].each do |k|
+              node.set[:resolved_ssh_keys][user] = node.resolved_ssh_keys[user] + node.ssh_keys[k]["keys"]
+            end
           end
         end
       end
