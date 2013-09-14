784b5fe1fce40bb20dba6145c70ff093c3fe8417
Bertrand Paquet <bpaquet@octo.com>
2013-09-14 16:50:14 +0200

    Do not reinstall key for percona each time

--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -3,7 +3,7 @@ if node.mysql.use_percona && node.apt.master_chef_add_apt_repo
 
   add_apt_repository "percona_repo" do
     url "http://repo.percona.com/apt"
-    key "1C4CBDCDCD2EFD2A"
+    key "CD2EFD2A"
     key_server "keys.gnupg.net"
   end
 
