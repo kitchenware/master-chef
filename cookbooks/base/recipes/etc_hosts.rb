5b0f92f2676936cc5d99aefd81375b3c4409bcc6
Bertrand Paquet <bpaquet@octo.com>
2013-09-26 21:34:49 +0200

    Allow to add some lines in etc hosts

--- cookbooks/base/recipes/etc_hosts.rb
+++ cookbooks/base/recipes/etc_hosts.rb
@@ -2,5 +2,9 @@
 template "/etc/hosts" do
   mode '0644'
   source "hosts.erb"
-  variables :hostname => node.hostname, :fqdn => node[:fqdn] || ""
+  variables({
+    :hostname => node.hostname,
+    :fqdn => node[:fqdn] || "",
+    :extended_hosts => node[:extended_hosts] || "",
+  })
 end
