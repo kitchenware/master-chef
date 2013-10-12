3673be48aad1058e23bd39ae7534f723e1242aea
Bertrand Paquet <bertrand.paquet@gmail.com>
2013-10-12 21:56:00 +0200

    Fix already downloaded files during bootstrap

--- runtime/bootstrap.sh
+++ runtime/bootstrap.sh
@@ -139,8 +139,8 @@ if [ "$OMNIBUS_DEB" = "" ]; then
   exit 42
 fi
 
-exec_command_chef "[ -f $OMNIBUS_DEB ] || $PROXY curl -f -s -L \"$OMNIBUS_DEB\" -o `basename $OMNIBUS_DEB`"
-exec_command_chef "sudo dpkg -i `basename $OMNIBUS_DEB`"
+exec_command_chef "[ -f `basename $OMNIBUS_DEB` ] || $PROXY curl -f -s -L \"$OMNIBUS_DEB\" -o `basename $OMNIBUS_DEB`"
+exec_command_chef "$SUDO dpkg -i `basename $OMNIBUS_DEB`"
 
 exec_command "$SUDO mkdir -p /opt/master-chef/etc"
 install_master_chef_file "cookbooks/master_chef/templates/default/solo.rb.erb" "/opt/master-chef/etc/solo.rb"
