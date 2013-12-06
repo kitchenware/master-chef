600d1be85c80ac6f3b02011a5241f312eb451c30
Bertrand Paquet <bertrand.paquet@gmail.com>
2013-12-07 00:12:12 +0100

    Fix git url

--- cookbooks/kibana/attributes/default.rb
+++ cookbooks/kibana/attributes/default.rb
@@ -16,7 +16,7 @@ default[:kibana] = {
 }
 
 default[:kibana3] = {
-  :git => 'http://github.com/elasticsearch/kibana.git',
+  :git => 'git://github.com/elasticsearch/kibana.git',
   :version => '4f1b33b316b7603ffa122296b2c2eee037ee8cfc',
   :directory => '/opt/kibana3',
   :location => '/kibana3',
