442772639d7bb022e48c43e274ada758a920da51
Bertrand Paquet <bertrand.paquet@gmail.com>
2014-03-17 20:35:28 +0100

    Allow to install apt keys from url

--- cookbooks/base/definitions/add_apt_repository.rb
+++ cookbooks/base/definitions/add_apt_repository.rb
@@ -1,19 +1,31 @@
 
 define :add_apt_key, {
   :key_server => nil,
+  :key_url => nil,
 } do
   add_apt_key_params = params
 
-  raise "Please specify key_server with add_apt_key" unless add_apt_key_params[:key_server]
+p add_apt_key_params
+  raise "Please specify key_server or key_url with add_apt_key" unless add_apt_key_params[:key_server] || add_apt_key_params[:key_url]
 
   unless node.apt_keys.include? add_apt_key_params[:name]
 
-    params = ""
-    params += "--keyserver-options http-proxy=#{ENV['BACKUP_http_proxy']}" if ENV['BACKUP_http_proxy']
+    if add_apt_key_params[:key_server]
+      params = ""
+      params += "--keyserver-options http-proxy=#{ENV['BACKUP_http_proxy']}" if ENV['BACKUP_http_proxy']
 
-    execute "add apt key #{add_apt_key_params[:name]}" do
-      command "apt-key adv #{params} --keyserver #{add_apt_key_params[:key_server]} --recv-keys #{add_apt_key_params[:name]}"
-      not_if "apt-key list | grep #{add_apt_key_params[:name]}"
+      execute "add apt key #{add_apt_key_params[:name]}" do
+        command "apt-key adv #{params} --keyserver #{add_apt_key_params[:key_server]} --recv-keys #{add_apt_key_params[:name]}"
+        not_if "apt-key list | grep #{add_apt_key_params[:name]}"
+      end
+    end
+
+    if add_apt_key_params[:key_url]
+      execute "add apt key #{add_apt_key_params[:name]} from #{add_apt_key_params[:key_url]}" do
+        command "curl #{add_apt_key_params[:key_url]} | apt-key add -"
+        environment get_proxy_environment
+        not_if "apt-key list | grep #{add_apt_key_params[:name]}"
+      end
     end
 
     node.set[:apt_keys] = node.apt_keys + [add_apt_key_params[:name]]
@@ -29,6 +41,7 @@ define :add_apt_repository, {
   :components => ["main"],
   :key => nil,
   :key_server => nil,
+  :key_url => nil,
   :run_apt_get_update => true,
 } do
   add_apt_repository_params = params
@@ -37,10 +50,11 @@ define :add_apt_repository, {
 
   add_apt_repository_params[:distrib] = %x{lsb_release -cs}.strip unless add_apt_repository_params[:distrib]
 
-  if add_apt_repository_params[:key] && add_apt_repository_params[:key_server]
+  if add_apt_repository_params[:key]
 
     add_apt_key add_apt_repository_params[:key] do
       key_server add_apt_repository_params[:key_server]
+      key_url add_apt_repository_params[:key_url]
     end
 
   end
