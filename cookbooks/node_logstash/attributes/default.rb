f6fa89392bb79cb3dc4cbd1b3a6c71c92d9206d5
Bertrand Paquet <bertrand.paquet@gmail.com>
2014-04-28 10:23:11 +0200

    Update node version for node-logstash

--- cookbooks/node_logstash/attributes/default.rb
+++ cookbooks/node_logstash/attributes/default.rb
@@ -5,7 +5,7 @@ default[:node_logstash] = {
   :config_directory => '/etc/logstash.d',
   :git => 'git://github.com/bpaquet/node-logstash.git',
   :version => 'f09ee5b01645cc2231e7a3025cb8a5d3ed9b66e1',
-  :node_version => '0.10.24',
+  :node_version => '0.10.26',
   :log_level => 'info',
   :patterns_directories => [],
 }
