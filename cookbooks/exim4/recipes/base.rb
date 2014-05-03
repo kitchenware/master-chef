7149bbb39cfea5ffeb646238375a7ba20fcb491f
Bertrand Paquet <bertrand.paquet@gmail.com>
2014-05-03 15:50:15 +0200

    Allow to configure passed file for jenkins

--- cookbooks/exim4/recipes/base.rb
+++ cookbooks/exim4/recipes/base.rb
@@ -7,3 +7,10 @@ service "exim4" do
   supports :status => true, :reload => true, :restart => true
   action auto_compute_action
 end
+
+if node.exim4[:passwd]
+	file "/etc/exim4/passwd.client" do
+		content node.exim4.passwd.join("\n")
+		notifies :restart, "service[exim4]"
+	end
+end
