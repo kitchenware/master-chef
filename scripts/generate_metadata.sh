6e056ffbd226976c679bcd2ffec431705a61ea74
Bertrand Paquet <bertrand.paquet@gmail.com>
2014-05-12 16:37:22 +0200

    Fix edge case in generate_metadata

--- scripts/generate_metadata.sh
+++ scripts/generate_metadata.sh
@@ -5,7 +5,7 @@ for dir in $@; do
     echo "Processing $dir/$i"
     echo "name '$i'" > $dir/$i/metadata.rb
     echo "" >> $dir/$i/metadata.rb
-    (grep -r "include_recipe" $dir/$i | perl -pe 's/^.*include_recipe ["'\''](.*)["'\''].*$/\1/g' | perl -pe 's/(.*)::.*/\1/g' | grep -v $i | sort | uniq | while read j; do
+    (grep -r "include_recipe" $dir/$i | perl -pe 's/^.*include_recipe ["'\'']([^"'\'']*)["'\''].*$/\1/g' | perl -pe 's/(.*)::.*/\1/g' | grep -v $i | sort | uniq | while read j; do
         echo "depends \"$j\""
     done) >> $dir/$i/metadata.rb
   done
