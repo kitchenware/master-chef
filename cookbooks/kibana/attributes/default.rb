45c3541cd72179fe3e61c9bced2fb503360f0db0
Bertrand Paquet <bertrand.paquet@gmail.com>
2014-06-06 18:22:35 +0200

    Kibana update

--- cookbooks/kibana/attributes/default.rb
+++ cookbooks/kibana/attributes/default.rb
@@ -1,7 +1,7 @@
 
 default[:kibana3] = {
   :url => 'http://download.elasticsearch.org/kibana/kibana/kibana-',
-  :version => '3.0.0milestone4',
+  :version => '3.1.0',
   :directory => '/opt/kibana3',
   :location => '/kibana3',
 }
