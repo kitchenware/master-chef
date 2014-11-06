f777a1e8dbbde701033f516da4d69e72f45d9253
Bertrand Paquet <bertrand.paquet@gmail.com>
2014-11-06 15:29:21 +0100

    Compatibility with Ohai 7

--- ohai_plugins/lxc.rb
+++ ohai_plugins/lxc.rb
@@ -1,10 +1,30 @@
 
-require_plugin "virtualization"
+if Ohai::VERSION =~ /^6/
 
-if File.exist?("/proc/1/environ")
-proc_environ = File.read("/proc/1/environ")
-  if proc_environ.match('lxc-start')
-    virtualization[:system] = "linux-lxc"
-    virtualization[:role] = "guest"
+  require_plugin "virtualization"
+
+  if File.exist?("/proc/1/environ")
+  proc_environ = File.read("/proc/1/environ")
+    if proc_environ.match('lxc')
+      virtualization[:system] = "linux-lxc"
+      virtualization[:role] = "guest"
+    end
   end
+
+else
+
+  Ohai.plugin(:Lxc) do
+    depends "virtualization"
+
+    collect_data(:default) do
+      if File.exist?("/proc/1/environ")
+        proc_environ = File.read("/proc/1/environ")
+        if proc_environ.match('lxc')
+          virtualization[:system] = "linux-lxc"
+          virtualization[:role] = "guest"
+        end
+      end
+    end
+  end
+
 end
--- ohai_plugins/override_ohai.rb
+++ ohai_plugins/override_ohai.rb
@@ -5,9 +5,30 @@ if File.exists? "/opt/master-chef/etc/override_ohai.json"
 
   override_ohai = JSON.load(File.read("/opt/master-chef/etc/override_ohai.json"))
 
-  override_ohai.each do |k, v|
-    require_plugin k
-    get_attribute(k).merge! v
+  if Ohai::VERSION =~ /^6/
+
+    override_ohai.each do |k, v|
+      require_plugin k
+      get_attribute(k).merge! v
+    end
+
+  else
+
+    Ohai.plugin(:OverrideOhai) do
+
+      override_ohai.each do |k, v|
+        provides k
+        depends k
+      end
+
+      collect_data(:default) do
+        override_ohai.each do |k, v|
+          get_attribute(k).merge! v
+        end
+      end
+
+    end
+
   end
 
 end
