1de6969f575ce021306ed9eb41f09173ac50977a
Bertrand Paquet <bertrand.paquet@gmail.com>
2014-11-07 15:13:55 +0100

    More fix in ohai 7 plugin

--- ohai_plugins/override_ohai.rb
+++ ohai_plugins/override_ohai.rb
@@ -1,32 +1,30 @@
 
 require 'json'
 
-if File.exists? "/opt/master-chef/etc/override_ohai.json"
+override_ohai = {}
 
-  override_ohai = JSON.load(File.read("/opt/master-chef/etc/override_ohai.json"))
+override_ohai = JSON.load(File.read("/opt/master-chef/etc/override_ohai.json")) if File.exists? "/opt/master-chef/etc/override_ohai.json"
 
-  if Ohai::VERSION =~ /^6/
+if Ohai::VERSION =~ /^6/
+
+  override_ohai.each do |k, v|
+    require_plugin k
+    get_attribute(k).merge! v
+  end
+
+else
+
+  Ohai.plugin(:OverrideOhai) do
 
     override_ohai.each do |k, v|
-      require_plugin k
-      get_attribute(k).merge! v
+      provides k
+      depends k
     end
 
-  else
-
-    Ohai.plugin(:OverrideOhai) do
-
+    collect_data(:default) do
       override_ohai.each do |k, v|
-        provides k
-        depends k
+        get_attribute(k).merge! v
       end
-
-      collect_data(:default) do
-        override_ohai.each do |k, v|
-          get_attribute(k).merge! v
-        end
-      end
-
     end
 
   end
