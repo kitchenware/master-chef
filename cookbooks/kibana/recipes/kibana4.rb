a3c89e2f20e1722c14b82fe5681c9c53b861dc44
Bertrand Paquet <bertrand.paquet@gmail.com>
2014-12-02 15:01:54 +0100

    Do not use supervisor to manage kibana

--- cookbooks/kibana/recipes/kibana4.rb
+++ cookbooks/kibana/recipes/kibana4.rb
@@ -1,25 +1,8 @@
 
 base_user "kibana"
 
-supervisor_worker "kibana" do
-	user "kibana"
-	workers 1
-	command "#{node.kibana4.directory}/shared/run.sh"
-end
-
 capistrano_app node.kibana4.directory do
-  user "root"
-end
-
-file "#{node.kibana4.directory}/shared/run.sh" do
-	mode '0755'
-	owner "kibana"
-	content <<-EOF
-#!/bin/sh
-
-cd #{node.kibana4.directory}/current
-bin/kibana
-EOF
+  user "kibana"
 end
 
 execute_version "install kibana 4" do
@@ -27,4 +10,36 @@ execute_version "install kibana 4" do
 	environment get_proxy_environment
 	version node.kibana4.version
 	file_storage "#{node.kibana4.directory}/.kibana_version"
+end
+
+file "#{node.kibana4.directory}/shared/run.sh" do
+	mode '0755'
+	owner "kibana"
+	content <<-EOF
+#!/bin/bash
+
+trap 'kill $(jobs -p) 2> /dev/null' EXIT
+
+cd #{node.kibana4.directory}/current
+bin/kibana >> #{node.kibana4.directory}/shared/log/kibana.log 2>&1
+
+EOF
+end
+
+basic_init_d "kibana" do
+	daemon "#{node.kibana4.directory}/shared/run.sh"
+	user "kibana"
+	make_pidfile true
+	background true
+end
+
+if node.logrotate[:auto_deploy]
+
+  logrotate_file "kibana" do
+    files [
+      "#{node.kibana4.directory}/shared/log/kibana.log"
+    ]
+    variables :copytruncate => true, :user => "kibana"
+  end
+
 end
