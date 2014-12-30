bd70d1ac62e9bae646dfa1386c4a221f8b16ffcf
Bertrand Paquet <bertrand.paquet@gmail.com>
2014-12-30 22:41:03 +0100

    Fix pam config for limits.conf

--- cookbooks/base/recipes/limits.rb
+++ cookbooks/base/recipes/limits.rb
@@ -1,9 +1,5 @@
 
-if node.platform == 'debian'
-
-  execute "active limits in pam common-session" do
-    command 'echo -e "session required pam_limits.so" >> /etc/pam.d/common-session'
-    not_if 'grep pam_limits.so /etc/pam.d/common-session'
-  end
-
+execute "active limits in pam common-session" do
+  command 'echo "session required pam_limits.so" >> /etc/pam.d/common-session-noninteractive'
+  not_if 'grep pam_limits.so /etc/pam.d/common-session'
 end
