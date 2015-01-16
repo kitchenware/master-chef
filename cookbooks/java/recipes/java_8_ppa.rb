3fffaa31bfb313a8ed64740d0fe6659e737abf8d
Bertrand Paquet <bertrand.paquet@gmail.com>
2015-01-16 17:39:00 +0100

    Improve java8 recipe

--- cookbooks/java/recipes/java_8_ppa.rb
+++ cookbooks/java/recipes/java_8_ppa.rb
@@ -1,13 +1,20 @@
-execute "accept oracle licence for java8" do
-  command "echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections"
-  not_if "sudo debconf-show oracle-java8-installer | grep \"accepted-oracle-license\" | grep true"
+if node.platform == "ubuntu" && node.apt.master_chef_add_apt_repo
+
+  execute "accept oracle licence for java8" do
+    command "echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections"
+    not_if "sudo debconf-show oracle-java8-installer | grep \"accepted-oracle-license\" | grep true"
+  end
+
+  add_apt_repository "ppa_java8" do
+    url "http://ppa.launchpad.net/webupd8team/java/ubuntu"
+    key "EEA14886 "
+    key_server "keyserver.ubuntu.com"
+  end
+
+  package "oracle-java8-installer"
+
+else
+
+  raise "Java 8 install is not implemented for your distro"
+
 end
-
-add_apt_repository "ppa_java8" do
-  url "http://ppa.launchpad.net/webupd8team/java/ubuntu"
-  key "EEA14886 "
-  key_server "keyserver.ubuntu.com"
-end
-
-package "oracle-java8-installer"
-
