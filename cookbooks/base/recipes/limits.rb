1a06c0796d12434195d7330a8652ccdae5c06883
Bertrand Paquet <bertrand.paquet@gmail.com>
2015-01-18 23:21:01 +0100

    Oups

--- cookbooks/base/recipes/limits.rb
+++ cookbooks/base/recipes/limits.rb
@@ -1,5 +1,5 @@
 
 execute "active limits in pam common-session" do
   command 'echo "session required pam_limits.so" >> /etc/pam.d/common-session-noninteractive'
-  not_if 'grep pam_limits.so /etc/pam.d/common-session'
+  not_if 'grep pam_limits.so /etc/pam.d/common-session-noninteractive'
 end
