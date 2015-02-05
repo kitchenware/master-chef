57642ab89056e57e65032a39ed76a70044aec41c
Bertrand Paquet <bertrand.paquet@gmail.com>
2015-02-05 12:03:35 +0100

    Add regex filter when purging cron.d and logrotate

--- cookbooks/cron/recipes/purge.rb
+++ cookbooks/cron/recipes/purge.rb
@@ -4,10 +4,11 @@ delayed_exec "Remove useless cron" do
   block do
     updated = false
     crons = find_resources_by_name_pattern(/^\/etc\/cron.d\/.*$/).map{|r| r.name}
+    regex_filter = node.cron[:no_purge_matching_crons] || []
     Dir["/etc/cron.d/*"].each do |n|
       Kernel.system "dpkg -S #{n} > /dev/null 2>&1"
       is_system_file = $?.exitstatus == 0
-      unless is_system_file || crons.include?(n)
+      unless is_system_file || crons.include?(n) || regex_filter.inject(false) {|current, x| current || Regexp.new(x).match(File.basename(n))}
         Chef::Log.info "Removing cron #{n}"
         File.unlink n
         updated = true
--- cookbooks/logrotate/recipes/default.rb
+++ cookbooks/logrotate/recipes/default.rb
@@ -15,8 +15,9 @@ unless node.logrotate[:disable_conf_purge]
   delayed_exec "Remove useless logrotate files" do
     block do
       files = find_resources_by_name_pattern(/^\/etc\/logrotate.d\/.*$/).map{|r| r.name}
+      regex_filter = node.logrotate[:no_purge_matching_logrotate] || []
       Dir["/etc/logrotate.d/*"].each do |n|
-        unless files.include? n
+        unless files.include?(n) || regex_filter.inject(false) {|current, x| current || Regexp.new(x).match(File.basename(n))}
           Kernel.system "dpkg -S #{n} > /dev/null 2>&1"
           if $?.exitstatus == 1
             Chef::Log.info "Removing logrotate file #{n}"
