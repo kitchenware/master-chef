91a9baeb2fb969714dd3a914ac74cbe7a99bd196
Gabriel Flory <gabrielflory@MBP-de-Gabriel.paris.netco>
2015-02-19 17:42:21 +0100

    feat(cookbooks) Enabled log configuration for worker processes
    -Added log_maxfiles and log_maxbytes parameters in the supervisor_worker definition
     stdout and stderr logfiles are configured identically using these values
    -Default attributes added, 10 maxfiles 10M per file
    -Worker template updated
    Should not break any previous implementations (due to default attributes)

--- cookbooks/supervisor/attributes/default.rb
+++ cookbooks/supervisor/attributes/default.rb
@@ -1,3 +1,5 @@
 default[:supervisor][:log_dir] = "/var/log/supervisor"
+default[:supervisor][:log_maxbytes] = "10M"
+default[:supervisor][:log_maxfiles] = 10
 default[:supervisor][:service_name] = "supervisord"
 default[:supervisor][:restart_delay_by_job] = 4
