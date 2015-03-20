972d0f905e0937cc8ac41fabbd2a5e015bc4f47e
Bertrand Paquet <bertrand.paquet@gmail.com>
2015-03-20 23:40:02 +0100

    Allow to add code before starting supervisor

--- cookbooks/supervisor/attributes/default.rb
+++ cookbooks/supervisor/attributes/default.rb
@@ -1,3 +1,6 @@
+
 default[:supervisor][:log_dir] = "/var/log/supervisor"
 default[:supervisor][:service_name] = "supervisord"
 default[:supervisor][:restart_delay_by_job] = 4
+
+default[:supervisor][:before_start_code] = {}
