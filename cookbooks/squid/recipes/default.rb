d65f6935170dd025266ee34cb30a7540569a9f89
mikrob <mr@finexkap.com>
2015-04-07 15:17:08 +0200

    use upstart if ubuntu trusty

--- cookbooks/squid/recipes/default.rb
+++ cookbooks/squid/recipes/default.rb
@@ -2,6 +2,7 @@
 package "squid"
 
 service "squid3" do
+  provider Chef::Provider::Service::Upstart if (platform?("ubuntu") && node.lsb.codename == "trusty")
   supports :restart => true
   action auto_compute_action
 end
