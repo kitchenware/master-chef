7e7b6fb2878825c45b30a2b07c62847505a0ecb2
Bertrand Paquet <bertrand.paquet@gmail.com>
2015-07-11 22:04:14 +0200

    Update ES

--- cookbooks/elasticsearch/attributes/default.rb
+++ cookbooks/elasticsearch/attributes/default.rb
@@ -1,7 +1,7 @@
 
 default[:elasticsearch] = {
   :user => 'elastic',
-  :url => 'http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.4.tar.gz',
+  :url => 'http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.6.0.tar.gz',
   :directory => '/opt/elasticsearch',
   :directory_data => '/opt/elasticsearch_data',
   :directory_logs => '/opt/elasticsearch/logs',
