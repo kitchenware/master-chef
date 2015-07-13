c8e0af3344aeeedc5e3f264ef57a82e5980e1209
Bertrand Paquet <bertrand.paquet@gmail.com>
2015-07-13 17:10:04 +0200

    Update to 3.0.4

--- cookbooks/redmine/attributes/default.rb
+++ cookbooks/redmine/attributes/default.rb
@@ -1,12 +1,13 @@
 default[:redmine] = {
 	:user => "redmine",
 	:directory => "/opt/redmine",
-	:version => "2.6.6",
+	:version => "3.0.4",
 	:git_url => "git://github.com/edavis10/redmine.git",
 	:location => "/redmine",
 	:configure_nginx => true,
 	:google_apps => false,
-	:google_apps_plugin_git => "https://github.com/twinslash/redmine_omniauth_google.git",
+	:google_apps_plugin_git => "https://github.com/zumbrunnen/redmine_omniauth_google.git",
+  :google_apps_plugin_branch => "zumbrunnen-redmine-3x-fix",
 	:database => {
   	:host => "localhost",
   	:username => "redmine",
--- cookbooks/redmine/recipes/google_apps.rb
+++ cookbooks/redmine/recipes/google_apps.rb
@@ -2,6 +2,6 @@
 git_clone "#{node.redmine.directory}/current/plugins/#{File.basename(node.redmine.google_apps_plugin_git).split(".").first}" do
   user node.redmine.user
   repository node.redmine.google_apps_plugin_git
-  reference "master"
+  reference node.redmine.google_apps_plugin_branch
   notifies :restart, "service[redmine]"
 end
