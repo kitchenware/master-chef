a78b865bde2f292ab8b58ddc4592c7ebf74d95d2
Bertrand Paquet <bertrand.paquet@gmail.com>
2015-07-13 16:19:13 +0200

    Code cleaning in redmine recipe

--- cookbooks/redmine/recipes/default.rb
+++ cookbooks/redmine/recipes/default.rb
@@ -80,31 +80,3 @@ ruby_rbenv_command "initialize redmine" do
   version node.redmine.version
   notifies :restart, "service[redmine]"
 end
-
-
-if node.redmine.google_apps
-
-  git_clone "#{node.redmine.directory}/current/plugins/google_apps" do
-    user node.redmine.user
-    repository node.redmine.google_apps_plugin_git
-    reference "master"
-    notifies :restart, "service[redmine]"
-  end
-
-  ruby_rbenv_command "initialize google apps plugin" do
-    user node.redmine.user
-    directory "#{node.redmine.directory}/current"
-    code "RAILS_ENV=production rake redmine:plugins:assets"
-    environment get_proxy_environment
-    file_storage "#{node.redmine.directory}/current/.gapps_ready"
-    version node.redmine.version
-    notifies :restart, "service[redmine]"
-  end
-
-  file "#{node.redmine.directory}/current/config/initializers/openid.rb" do
-    content "OpenIdAuthentication.store = :file"
-    notifies :restart, "service[redmine]"
-  end
-
-end
-
