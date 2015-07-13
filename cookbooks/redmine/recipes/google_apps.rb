bf1ba934c1d9290ddc7d7099983fa0cedec9b181
Bertrand Paquet <bertrand.paquet@gmail.com>
2015-07-13 16:56:52 +0200

    Fix plugin install

--- cookbooks/redmine/recipes/google_apps.rb
+++ cookbooks/redmine/recipes/google_apps.rb
@@ -1,5 +1,5 @@
 
-git_clone "#{node.redmine.directory}/current/plugins/google_apps" do
+git_clone "#{node.redmine.directory}/current/plugins/#{File.basename(node.redmine.google_apps_plugin_git).split(".").first}" do
   user node.redmine.user
   repository node.redmine.google_apps_plugin_git
   reference "master"
