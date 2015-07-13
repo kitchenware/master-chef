2d05531f419d3d7bb953fe12ada489a251d63571
Bertrand Paquet <bertrand.paquet@gmail.com>
2015-07-13 16:40:02 +0200

    Update redmine

--- cookbooks/redmine/attributes/default.rb
+++ cookbooks/redmine/attributes/default.rb
@@ -1,12 +1,12 @@
 default[:redmine] = {
 	:user => "redmine",
 	:directory => "/opt/redmine",
-	:version => "2.3.3",
+	:version => "2.6.6",
 	:git_url => "git://github.com/edavis10/redmine.git",
 	:location => "/redmine",
 	:configure_nginx => true,
 	:google_apps => false,
-	:google_apps_plugin_git => "https://github.com/mikrob/redmine_google_apps.git",
+	:google_apps_plugin_git => "https://github.com/twinslash/redmine_omniauth_google.git",
 	:database => {
   	:host => "localhost",
   	:username => "redmine",
