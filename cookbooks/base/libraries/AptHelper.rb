81789586597f400cd181c44dbf73157d187c5e66
Bertrand Paquet <bertrand.paquet@gmail.com>
2015-09-09 23:46:43 +0200

    Fix apt helper

--- cookbooks/base/libraries/AptHelper.rb
+++ cookbooks/base/libraries/AptHelper.rb
@@ -2,7 +2,7 @@
 module AptHelper
 
   def self.backports_version package
-    %x{apt-get changelog -s -t $(lsb_release -sc)-backports #{package}  | grep -v ^Get | head -n1}.match(/\(([^\)]+)\)/)[1]
+    %x{apt-get changelog -s -t $(lsb_release -sc)-backports #{package}  | grep $(lsb_release -sc)-backports | head -n1}.match(/\(([^\)]+)\)/)[1]
   end
 
   def self.not_installed package
