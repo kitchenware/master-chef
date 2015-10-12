c8c1f8f3719127f499e228eb20a905153d85a4bc
Bertrand Paquet <bertrand.paquet@gmail.com>
2015-10-12 12:38:55 +0200

    Allow some tweaking on resolv_conf management

--- cookbooks/base/recipes/resolv_conf.rb
+++ cookbooks/base/recipes/resolv_conf.rb
@@ -1,10 +1,10 @@
 
-if node[:resolv_conf]
+if node[:resolv_conf] && ! node[:no_resolv_conf_update]
 
   template "/etc/resolv.conf" do
     source "resolv.conf.erb"
     mode '0644'
-    variables :resolv => node.resolv_conf
+    variables :resolv => node[:override_resolv_conf] || node.resolv_conf
   end
 
 end
