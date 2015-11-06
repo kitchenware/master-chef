5ba5736376bfe5fd2f43559617c00f99e5994fea
Bertrand Paquet <bertrand.paquet@gmail.com>
2015-11-06 16:41:48 +0100

    Use http for getting mongodb key

--- cookbooks/mongodb/recipes/server.rb
+++ cookbooks/mongodb/recipes/server.rb
@@ -5,7 +5,7 @@ if node.platform == "ubuntu" && node.apt.master_chef_add_apt_repo
 		distrib "dist"
 		components ['10gen']
 		key "7F0CEB10"
-		key_server "keyserver.ubuntu.com"
+		key_url "https://docs.mongodb.org/10gen-gpg-key.asc"
 		run_apt_get_update true
 	end
 end
@@ -17,7 +17,7 @@ if node.platform == "debian" && node.apt.master_chef_add_apt_repo
 		distrib "dist"
 		components ['10gen']
 		key "7F0CEB10"
-		key_server "keyserver.ubuntu.com"
+		key_url "https://docs.mongodb.org/10gen-gpg-key.asc"
 		run_apt_get_update true
 	end
 end
