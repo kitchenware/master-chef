22a7d73ec238203efbcbd5197f86f6e54c5d8164
Bertrand Paquet <bertrand.paquet@gmail.com>
2015-11-27 15:04:03 +0100

    Allow to disable dump attributes

--- cookbooks/master_chef/recipes/init.rb
+++ cookbooks/master_chef/recipes/init.rb
@@ -1,7 +1,9 @@
 
-ruby_block "dump_attributes" do
-  block do
-    FileUtils.mkdir_p "/opt/master-chef/var/last"
-    File.write("/opt/master-chef/var/last/last_attributes.json", JSON.dump(node.to_hash), {:perm => "0600".to_i(8)})
+unless node[:no_dump_attributes]
+  ruby_block "dump_attributes" do
+    block do
+      FileUtils.mkdir_p "/opt/master-chef/var/last"
+      File.write("/opt/master-chef/var/last/last_attributes.json", JSON.dump(node.to_hash), {:perm => "0600".to_i(8)})
+    end
   end
 end
