34207e73bfb848cdb0af1e91a9c17787f3330eb5
Bertrand Paquet <bertrand.paquet@gmail.com>
2016-01-05 18:00:13 +0100

    Upgrade kibana

--- cookbooks/kibana/attributes/default.rb
+++ cookbooks/kibana/attributes/default.rb
@@ -1,14 +1,7 @@
 
-default[:kibana3] = {
-  :url => 'http://download.elasticsearch.org/kibana/kibana/kibana-',
-  :version => '3.1.0',
-  :directory => '/opt/kibana3',
-  :location => '/kibana3',
-}
-
-default[:kibana4] = {
+default[:kibana] = {
 	:url => 'https://download.elasticsearch.org/kibana/kibana/kibana-',
-  :version => '4.1.2-linux-x64',
+  :version => '4.3.1-linux-x64',
   :directory => '/opt/kibana4',
   :location => '/kibana4',
   :opts => '',
--- cookbooks/kibana/recipes/default.rb
+++ cookbooks/kibana/recipes/default.rb
@@ -1,11 +1,52 @@
 
-capistrano_app node.kibana3.directory do
-  user "root"
+base_user "kibana"
+
+capistrano_app node.kibana.directory do
+  user "kibana"
 end
 
-execute_version "install kibana" do
-	command "cd #{node.kibana3.directory} && rm -rf current && curl -L -f -s #{node.kibana3.url}#{node.kibana3.version}.tar.gz -o kibana.tar.gz && tar xvzf kibana.tar.gz && rm kibana.tar.gz && mv kibana-* current"
-	environment get_proxy_environment
-	version node.kibana3.version
-	file_storage "#{node.kibana3.directory}/.kibana_version"
+basic_init_d "kibana" do
+	daemon "#{node.kibana.directory}/shared/run.sh"
+	user "kibana"
+	make_pidfile true
+	background true
+  directory_check ["#{node.kibana.directory}/current"]
+end
+
+file "#{node.kibana.directory}/shared/run.sh" do
+  mode '0755'
+  owner "kibana"
+  notifies :restart, "service[kibana]"
+  content <<-EOF
+#!/bin/bash
+
+trap 'kill $(jobs -p) 2> /dev/null' EXIT
+
+cd #{node.kibana.directory}/current
+bin/kibana #{node.kibana.opts} >> #{node.kibana.directory}/shared/log/kibana.log 2>&1
+
+EOF
+end
+
+directory "#{node.kibana.directory}/current/optimize" do
+  owner "kibana"
+end
+
+execute_version "install kibana 4" do
+  command "cd #{node.kibana.directory} && rm -rf current && curl -L -f -s #{node.kibana.url}#{node.kibana.version}.tar.gz -o kibana.tar.gz && tar xvzf kibana.tar.gz && rm kibana.tar.gz && mv kibana-* current"
+  environment get_proxy_environment
+  version node.kibana.version
+  file_storage "#{node.kibana.directory}/.kibana_version"
+  notifies :restart, "service[kibana]"
+end
+
+if node.logrotate[:auto_deploy]
+
+  logrotate_file "kibana" do
+    files [
+      "#{node.kibana.directory}/shared/log/kibana.log"
+    ]
+    variables :copytruncate => true, :user => "kibana"
+  end
+
 end
