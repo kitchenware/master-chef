712263def495a2ed49b57deaba76a137d2a343bb
Bertrand Paquet <bertrand.paquet@gmail.com>
2016-02-08 15:40:15 +0100

    Later resolv_conf detection

--- cookbooks/base/recipes/resolv_conf.rb
+++ cookbooks/base/recipes/resolv_conf.rb
@@ -4,7 +4,6 @@ if node[:resolv_conf] && ! node[:no_resolv_conf_update]
   template "/etc/resolv.conf" do
     source "resolv.conf.erb"
     mode '0644'
-    variables :resolv => node[:override_resolv_conf] || node.resolv_conf
   end
 
 end
