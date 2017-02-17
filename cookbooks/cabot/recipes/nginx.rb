933a2c72fca5a522179d65b504906d951d5cca2f
Gauthier FRANCOIS <gauthier.francois@doctolib.fr>
2017-02-17 11:54:37 +0100

    chore: add cabot x-auth-tiken

--- cookbooks/cabot/recipes/nginx.rb
+++ cookbooks/cabot/recipes/nginx.rb
@@ -3,6 +3,14 @@ node.set[:nginx][:default_vhost][:enabled] = false
 
 include_recipe "nginx"
 
+conf = {
+  :path => "#{node.cabot.root}/current",
+  :cabot_port => node.cabot.nginx.cabot.cabot_port
+}
+
+conf[:google_auth_proxy_port] = node.cabot.google_auth_proxy_port
+conf[:token] = node.cabot.x_auth_token
+
 nginx_vhost "cabot:nginx:cabot" do
-  options :path => "#{node.cabot.root}/current", :cabot_port => node.cabot.nginx.cabot.cabot_port
+  options conf
 end
