9a78260d0cae1f4b6d06ad5cc7c241beb70f91fd
Adrien Waksberg <adrien.waksberg@doctolib.fr>
2017-04-24 11:54:20 +0200

    feat(do678): add es branch

--- cookbooks/elasticsearch/recipes/repo.rb
+++ cookbooks/elasticsearch/recipes/repo.rb
@@ -1,8 +1,9 @@
+branch = node[:elasticsearch].key?(:branch) ? node[:elasticsearch][:branch] : '2.x'
 
-add_apt_repository "elasticsearch" do
-  url "http://packages.elastic.co/elasticsearch/2.x/debian"
-  distrib "stable"
-  components ["main"]
-  key "D88E42B4"
-  key_url "https://packages.elastic.co/GPG-KEY-elasticsearch"
+add_apt_repository 'elasticsearch' do
+  url "http://packages.elastic.co/elasticsearch/#{branch}/debian"
+  distrib 'stable'
+  components ['main']
+  key 'D88E42B4'
+  key_url 'https://packages.elastic.co/GPG-KEY-elasticsearch'
 end
