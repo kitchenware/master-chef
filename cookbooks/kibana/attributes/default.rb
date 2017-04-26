b5be399a7b3271c5bb6b1110f48174c2fc3435a4
Adrien Waksberg <adrien.waksberg@doctolib.fr>
2017-04-26 13:51:03 +0200

    kibana: change download url

--- cookbooks/kibana/attributes/default.rb
+++ cookbooks/kibana/attributes/default.rb
@@ -1,8 +1,7 @@
-
 default[:kibana] = {
-	:url => 'https://download.elasticsearch.org/kibana/kibana/kibana-',
-  :version => '4.3.1-linux-x64',
-  :directory => '/opt/kibana4',
-  :location => '/kibana4',
-  :opts => '',
+  url: 'https://artifacts.elastic.co/downloads/kibana/kibana-',
+  version: '4.3.1-linux-x64',
+  directory: '/opt/kibana4',
+  location: '/kibana4',
+  opts: ''
 }
