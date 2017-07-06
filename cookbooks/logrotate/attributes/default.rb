4703bcc9a638935b4afef4196bb246ee9c5a123b
flaviogurgel <flavio.araque-gurgel@doctolib.fr>
2017-07-06 17:03:22 +0200

    New modifiable parameter for logrotate

--- cookbooks/logrotate/attributes/default.rb
+++ cookbooks/logrotate/attributes/default.rb
@@ -2,11 +2,12 @@
 default[:logrotate][:files] = {}
 
 default[:logrotate][:default_config] = {
-	:rotate => 52,
-	:frequency => 'daily',
-	:user => 'root',
-	:group => 'root',
-	:mode => '644',
+  :rotate => 52,
+  :frequency => 'daily',
+  :user => 'root',
+  :group => 'root',
+  :mode => '644',
+  :delaycompress => true
 }
 
 default[:logrotate][:disable_conf_purge] = false
