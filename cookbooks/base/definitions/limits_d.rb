d46c7defc0b7d291a6ceedfed144f809c382ee29
Bertrand Paquet <bertrand.paquet@gmail.com>
2018-06-05 20:50:22 -0700

    Deploy lmits file immediately instead of waiting

--- cookbooks/base/definitions/limits_d.rb
+++ cookbooks/base/definitions/limits_d.rb
@@ -10,22 +10,13 @@ define :limits_d, {
   raise "Please specify limit with limits_d" unless limits_d_params[:limit]
   raise "Please specify value with limits_d" unless limits_d_params[:value]
 
-  if find_resources_by_name("/etc/security/limits.d/#{limits_d_params[:user]}.conf").empty?
-    template "/etc/security/limits.d/#{limits_d_params[:user]}.conf" do
-      variables :user => limits_d_params[:user]
-      source "limits_d.erb"
-      cookbook "base"
-      action :nothing
-    end
-  end
-
   node.set[:user_limits][limits_d_params[:user]] = {} unless node.user_limits[limits_d_params[:user]]
   node.set[:user_limits][limits_d_params[:user]][limits_d_params[:limit]] = [node.user_limits[limits_d_params[:user]][limits_d_params[:limit]] || 0, limits_d_params[:value]].max
 
-  ruby_block "update limits for #{limits_d_params[:user]} #{limits_d_params[:name]}" do
-    block do
-    end
-    notifies :create, "template[/etc/security/limits.d/#{limits_d_params[:user]}.conf]"
+  template "/etc/security/limits.d/#{limits_d_params[:user]}.conf" do
+    variables :user => limits_d_params[:user]
+    source "limits_d.erb"
+    cookbook "base"
   end
 
 end
