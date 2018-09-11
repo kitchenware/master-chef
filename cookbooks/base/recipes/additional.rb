1c83803ff20bc878c665dd3a69360ebafca2a74e
Gauthier FRANÇOIS <gauthier.francois@doctolib.com>
2018-09-11 11:43:17 +0200

    feat: add additionnal packages for stretch migration

--- cookbooks/base/recipes/additional.rb
+++ cookbooks/base/recipes/additional.rb
@@ -6,3 +6,13 @@ if node[:additional_packages]
   end
 
 end
+
+if node.lsb.codename == 'stretch' && node[:stretch_additional_packages]
+  node.stretch_additional_packages.each do |p|
+    package p
+  end
+elsif node[:default_additional_packages]
+  node.default_additional_packages.each do |p|
+    package p
+  end
+end
